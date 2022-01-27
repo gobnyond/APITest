211404851a686019afb8bb39ba8e40794a368acc
gobnyond <iong4524@naver.com>
2022-01-28 03:03:17 +0900

    Model

---
+++ APITest2/Controller/BookTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BookTableViewCell.swift
+//  ApiTest
+//
+//  Created by 정수빈 on 2022/01/28.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ APITest2/Model/Aladin.swift
@@ -0,0 +1,35 @@
+//
+//  Aladin.swift
+//  ApiTest
+//
+//  Created by 정수빈 on 2022/01/28.
+//
+
+import Foundation
+
+struct AladinOpen: Decodable {
+    var version: String
+    var logo: String
+    var title: String
+    var link: String
+    var pubDate: String
+    var totalResults: Int
+    var startIndex: Int
+    var itemsPerPage: Int
+    var query: String
+    var searchCategoryId: Int
+    var searchCategoryName: String
+    var item: BookData
+}
+
+struct BookData: Decodable {
+    var books: [Book]
+}
+
+struct Book: Decodable {
+    var cover: String
+    var title: String
+    var priceStandard: Int
+    var descriptin: String
+}
+// 이미지, 제목, 가격, 요약(모든 내용)
